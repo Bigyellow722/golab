af03560499fe5487beb260093c2e90ea06a86622
Bigyellow722 <lhkwqy@qq.com>
2018-11-05 20:15:46 +0800

    协程并发获取html

--- fetch/fetch.go
+++ fetch/fetch.go
@@ -26,3 +26,27 @@ func Fetch1(url string) {
 		os.Exit(1)
 	}
 }
+
+func Fetch2(url string, ch chan<- string) {
+	if !strings.HasPrefix(url, "http://") {
+		url = "http://" + url
+	}
+
+	//从url获取resp，如果出现错误将错误发送出去
+	//同时会阻塞该协程直到主协程从ch中把错误信息读出
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	fmt.Println(resp.Status)
+	nbytes, err := io.Copy(os.Stdout, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+	}
+
+	ch <- fmt.Sprintf("done and nbytes: %7d", nbytes)
+
+}
--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"os"
 	"test/fetch"
 )
 
 func main() {
+	/*
+		for _, url := range os.Args[1:] {
+			fetch.Fetch1(url)
+		}
+	*/
+
+	ch := make(chan string)
 	for _, url := range os.Args[1:] {
-		fetch.Fetch1(url)
+		go fetch.Fetch2(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
 	}
 }
