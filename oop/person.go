ad56d92fe11b9703dc56e289c8ba7b3b6f515f32
Bigyellow722 <lhkwqy@qq.com>
2019-01-23 21:11:22 +0800

    1.23

--- oop/person.go
+++ oop/person.go
@@ -2,28 +2,25 @@ package oop
 
 import "fmt"
 
-
 //1.封装
-type Person struct{
-	Name string
-	Sex int32
+type Person struct {
+	Name    string
+	Sex     int32
 	Address string
 }
 
-func NewPerson
-
-func (p Person)Publish(){
+func (p Person) Publish() {
 	fmt.Println("method : Publish")
 }
 
-func (p Person)Set(name string,sex int32,address string){
+func (p Person) Set(name string, sex int32, address string) {
 	p.Name = name
 	p.Sex = sex
 	p.Address = address
 }
 
-func (p Person)Get(){
-	fmt.Printf("%v who is %v,lives in %v",p.Name,p.Sex,p.Address)
+func (p Person) Get() {
+	fmt.Printf("%v who is %v,lives in %v", p.Name, p.Sex, p.Address)
 }
 
 //2.继承
@@ -31,4 +28,3 @@ type ChinaPerson struct {
 	Person
 	area string
 }
-
